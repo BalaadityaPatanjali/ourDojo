86a001b5b1f9963c83e7298d3ab43633aa5ba016
BalaadityaPatanjali <balaaditya.0808.com>
2026-01-01 03:03:22 +0530

    final changes done

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -2,9 +2,8 @@ package db
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"os"
-	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -12,30 +11,18 @@ import (
 var Pool *pgxpool.Pool
 
 func Connect() error {
-	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	connStr := os.Getenv("DATABASE_URL")
 
-	config, err := pgxpool.ParseConfig(dsn)
+	if connStr == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		return err
 	}
 
-	config.MaxConns = 10
-	config.MinConns = 2
-	config.MaxConnLifetime = time.Hour
-
-	pool, err := pgxpool.NewWithConfig(context.Background(), config)
-	if err != nil {
-		return err
-	}
-
-	// Test connection
+	// Optional but recommended: verify connection
 	if err := pool.Ping(context.Background()); err != nil {
 		return err
 	}
